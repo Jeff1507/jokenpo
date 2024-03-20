c515703f11ec56bd321e4ccf8e874cb75347bfd1
Jefferson <Dibloob@outlook.com>
2024-03-20 15:13:04 -0300

    feito

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Jogo(),
+    theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black)),
+  ));
+}
